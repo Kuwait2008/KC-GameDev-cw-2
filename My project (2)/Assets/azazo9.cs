5579f729dc0c36d884b474431cc6d6aa493b050e
Kuwait2008 <z.alsarraf.08@gmail.com>
2022-08-07 15:00:20 +0300

    cw2

---
+++ My project (2)/Assets/azazo9.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class azazo9 : MonoBehaviour
+{
+    int age = 17;
+    string name = "scotte";
+    bool iswerewolf = true;
+        float speed = 22.3f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("my age is" + age + " and my name is " + name);
+        print("the prophecy says that he is a " + iswerewolf + " werewolf , with a speed of "+ speed) ;
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
